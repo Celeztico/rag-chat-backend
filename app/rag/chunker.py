52635783b4d5edbff9a4f2743b10693f7819f48d
Celeztico <153655968+Celeztico@users.noreply.github.com>
2026-04-05 22:32:50 +0530

    updated chunking to dynamically adjust based on size of text

--- app/rag/chunker.py
+++ app/rag/chunker.py
@@ -1,11 +1,16 @@
-def chunk_text(text: str, size=500, overlap=100):
+def chunk_text(text: str, chunk_size=500, overlap=100):
+    # return if small text
+    if len(text) <= 800:
+        return [text]
+
+    # else chunking with overlap
     chunks = []
     start = 0 
 
     while start < len(text):
-        end = start + size
+        end = start + chunk_size
         chunk = text[start:end]
         chunks.append(chunk)
-        start = end - overlap
+        start += (chunk_size - overlap)
     
     return chunks
